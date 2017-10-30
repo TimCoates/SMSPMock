0dc316c58ce265e138e140bcb61a1385077789ef
Tim Coates <tim.coates@gmail.com>
2017-10-30 21:56:50 +0000

    Test page now loads suitable request body data.

--- Test.js
+++ Test.js
@@ -12,6 +12,9 @@
 //WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //See the License for the specific language governing permissions and
 //limitations under the License.
+var templates = require ('./templates.js');
+var mustache = require("mustache");
+const uuidv4 = require('uuid/v4');
 
 module.exports.entrypoint = (event, context, callback) => {
 
@@ -22,45 +25,98 @@ module.exports.entrypoint = (event, context, callback) => {
     if(typeof event.Records != 'undefined') {
         callback(null, null);
     } else {
-        var b64Data = "AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAGM7DwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAiIgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIiIiARERERERERIBEREREREREgERERERERESARERERERERD+HwAA/B8AAPwfAAD8HwAA/B8AAPwfAAD8HwAA/B8AAPwfAAD8HwAA/B8AAPwAAACAAAAAgAAAAIAAAACAAQAA";
 
-    	var body = "<html><head>" +
-    	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
-    	"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" " +
-        "integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">" +
-        "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>" +
-        "<link id=\"favicon\" rel=\"shortcut icon\" type=\"image/png\" href=\"data:​image/png;base64," + b64Data + "\">" +
-        "<link rel=\"stylesheet\" href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css\">" +
-        "<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js\"></script>" +
-    	"<title>SMSP Mock - Send tests</title></head><body>" +
-    	"<div class=\"container\">\n" +
-    	" <div class=\"jumbotron\"><h1><a href=\"Homepage\">Send tests</a></h1></div>\n" +
-        " <div class='row'><form action='#'>\n" +
-        "  <div class='col-sm-9'>\n" +
-        // Here we have a code window
-        "   <pre id= 'reqBlock' class='.pre-scrollable'></pre>" +
-        "  </div>" // Ends the wide column
-        "  <div class='col-sm-3'>\n" +
-        // Here we have a list of radio buttons, plus a Submit button
-        "   <div>\n" +
-        "    <input type='radio' name='request' id='request' value='0'>Request 0<br />\n" +
-        "    <input type='radio' name='request' id='request' value='1'>Request 1<br />\n" +
-        "    <input type='radio' name='request' id='request' value='2'>Request 2<br />\n" +
-        "    <input type='radio' name='request' id='request' value='3'>Request 3<br />\n" +
-        "</div>\n" +
-        "   <div><button id='selectRequest' type='button'>Submit!</button></div>\n" +
-        "   </form>\n" +
-        "  </div>\n" // Ends the narrow column
-        " </div>\n" + // Ends row
-    	"</div>\n" + // Ends Container
-        "</body>\n" +
-        "</html>";
+        var id = null;
 
-        var reply = {
-            "statusCode": 200,
-            "headers": { "Content-Type": "text/html" },
-            "body": body
-        };
-        callback(null, reply);
+        if(typeof event.queryStringParameters != 'undefined') {
+            if(event.queryStringParameters != null) {
+                if(typeof event.queryStringParameters.id != 'undefined') {
+                    id = event.queryStringParameters.id;
+                }
+            }
+        }
+
+        console.log("Requesting ID: " + id);
+
+        if(id == null) {
+
+            var b64Data = "AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAGM7DwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAiIgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIAAAAAAAEREgAAAAAAARESAAAAAAABERIiIiARERERERERIBEREREREREgERERERERESARERERERERD+HwAA/B8AAPwfAAD8HwAA/B8AAPwfAAD8HwAA/B8AAPwfAAD8HwAA/B8AAPwAAACAAAAAgAAAAIAAAACAAQAA";
+
+        	var body = "<html><head>" +
+        	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">" +
+        	"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css\" " +
+            "integrity=\"sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M\" crossorigin=\"anonymous\">" +
+            "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>" +
+            "<link id=\"favicon\" rel=\"shortcut icon\" type=\"image/png\" href=\"data:​image/png;base64," + b64Data + "\">" +
+            "<link rel=\"stylesheet\" href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css\">" +
+            "<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js\"></script>" +
+        	"<title>SMSP Mock - Send tests</title></head><body>" +
+        	"<div class=\"container\">\n" +
+        	" <div class=\"jumbotron\"><h1><a href=\"Homepage\">Send tests</a></h1></div>\n" +
+            " <div class='row'><form id='myForm' action='#'>\n" +
+            "  <div class='col' style='width:80%'>\n" +
+            // Here we have a code window
+            "   <figure class='highlight'><pre id='reqBody' class='.pre-scrollable'>" +
+            "   </pre></figure>" +
+            "  </div>" + // Ends the wide column
+            "  <div class='col' style='width:20%'>\n" +
+            // Here we have a list of radio buttons, plus a Submit button
+            "   <div>\n" +
+            "    <input type='radio' name='request' id='request' value='0'>getPatientDetails Partial match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='1'>getPatientDetails Full match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='2'>getPatientDetails No match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='3'>getPatientDetailsBySearch Match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='4'>getPatientDetailsByNHSNumber Match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='5'>getPatientDetailsByNHSNumber No match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='6'>getNHSNumber Match<br />\n" +
+            "    <input type='radio' name='request' id='request' value='7'>verifyNHSNumber Match<br />\n" +
+            "</div>\n" +
+            "   <div><button id='selectRequest' type='button'>Submit!</button></div>\n" +
+            "   </form>\n" +
+            "  </div>\n" + // Ends the narrow column
+            " </div>\n" + // Ends row
+        	"</div>\n" + // Ends Container
+            "</body>\n" +
+            "<script>\n" +
+            "$('#myForm input').on('change', function() {\n" +
+//            "   alert($('input[name=request]:checked', '#myForm').val());\n" +
+            "   var id = $('input[name=request]:checked', '#myForm').val()\n;" + 
+            "   var theUrl = window.location.href + '?id=' + id;\n" +
+//            "   console.log(theUrl);\n" +
+//            "   $.ajax({\n" +
+//            "       method: 'GET',\n" +
+//            "       url: theUrl\n" +
+//            "   })\n" +
+//            "   .done(function(msg) {\n" +
+//            "       console.log('Message: ' + msg);" +
+//            "       $('#reqBody').html = msg;\n" +
+//            "   });\n" +
+            "   $('#reqBody').load(theUrl);" +
+            "});\n" +
+            "</script>" +
+            "</html>";
+
+            var reply = {
+                "statusCode": 200,
+                "headers": { "Content-Type": "text/html" },
+                "body": body
+            };
+            callback(null, reply);
+        } else {
+            var tpl = templates.requestTemplates[id];
+
+            var msg_id = uuidv4();
+            var dataToWrap = {
+                msgID: msg_id
+            };
+
+            var body = mustache.render(tpl, dataToWrap);
+            var reply = {
+                "statusCode": 200,
+                "headers": { "Content-Type": "application/xml" },
+                "body": body
+            };
+            callback(null, reply);
+        }
     }
 };
--- templates.js
+++ templates.js
@@ -1,4 +1,5 @@
 var responseTemplates = [];
+var requestTemplates = [];
 //Copyright 2017 Tim Coates
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
@@ -163,9 +164,81 @@ var envelopeTemplate = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/s
 "</soap:Body>\n" +
 "</soap:Envelope>\n";
 
+requestTemplates[0] = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:wsa=\"http://www.w3.org/2005/08/addressing\" xmlns:itk=\"urn:nhs-itk:ns:201005\">\n" +
+"	<soap:Header>\n" +
+"		<wsa:MessageID>{{msgID}}</wsa:MessageID>\n" +
+"		<wsa:Action>urn:nhs-itk:services:201005:getNHSNumber-v1-0</wsa:Action>\n" +
+"		<wsa:To>https://192.168.54.6/smsp/pds</wsa:To>\n" +
+"		<wsa:From>\n" +
+"			<wsa:Address>192.168.54.7</wsa:Address>\n" +
+"		</wsa:From>\n" +
+"		<wsse:Security xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">\n" +
+"			<wsu:Timestamp xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\" wsu:Id=\"{{msgID}}\">\n" +
+"				<wsu:Created>2016-07-27T11:10:23Z</wsu:Created>\n" +
+"				<wsu:Expires>2020-07-27T11:20:23Z</wsu:Expires>\n" +
+"			</wsu:Timestamp>\n" +
+"			<wsse:UsernameToken>\n" +
+"				<wsse:Username>TKS Server test</wsse:Username>\n" +
+"			</wsse:UsernameToken>\n" +
+"		</wsse:Security>\n" +
+"	</soap:Header>\n" +
+"	<soap:Body>\n" +
+"		<itk:DistributionEnvelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+"			<itk:header service=\"urn:nhs-itk:services:201005:getNHSNumber-v1-0\" trackingid=\"{{msgID}}\">\n" +
+"				<itk:auditIdentity>\n" +
+"					<itk:id type=\"1.2.826.0.1285.0.2.0.107\" uri=\"868000003114\"/>\n" +
+"				</itk:auditIdentity>\n" +
+"				<itk:manifest count=\"1\">\n" +
+"					<itk:manifestitem id=\"uuid_{{msgID}}\" mimetype=\"text/xml\" profileid=\"urn:nhs-en:profile:getNHSNumberRequest-v1-0\" base64=\"false\" compressed=\"false\" encrypted=\"false\"/>\n" +
+"				</itk:manifest>\n" +
+"				<itk:senderAddress uri=\"urn:nhs-uk:addressing:ods:rhm:team1:C\"/>\n" +
+"			</itk:header>\n" +
+"			<itk:payloads count=\"1\">\n" +
+"				<itk:payload id=\"uuid_{{msgID}}\">\n" +
+"                <getPatientDetails-v1-0 xmlns=\"urn:hl7-org:v3\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" moodCode=\"EVN\" classCode=\"CACT\">\n" +
+"                    <id root=\"{{ guidManifestItem }}\"/>\n" +
+"                    <code codeSystem=\"2.16.840.1.113883.2.1.3.2.4.17.284\" code=\"getPatientDetailsRequest-v1-0\"/>\n" +
+"                    <queryEvent>\n" +
+"                        <Person.DateOfBirth>\n" +
+"                            <value value=\"20101010\"/>\n" +
+"                            <semanticsText>Person.DateOfBirth</semanticsText>\n" +
+"                        </Person.DateOfBirth>\n" +
+"                        <Person.NHSNumber>\n" +
+"                            <value root=\"2.16.840.1.113883.2.1.4.1\" extension=\"1231231234\"/>\n" +
+"                            <semanticsText>Person.NHSNumber</semanticsText>\n" +
+"                        </Person.NHSNumber>\n" +
+"                        <Person.Name>\n" +
+"                            <value>\n" +
+"                                <given>Fred</given>\n" +
+"                                <family>Bloggs</family>\n" +
+"                            </value>\n" +
+"                            <semanticsText>Person.Name</semanticsText>\n" +
+"                        </Person.Name>\n" +
+"                        <Person.Postcode>\n" +
+"                            <value>\n" +
+"                                <postalCode>LS1 4HR</postalCode>\n" +
+"                            </value>\n" +
+"                            <semanticsText>Person.Postcode</semanticsText>\n" +
+"                        </Person.Postcode>\n" +
+"                    </queryEvent>\n" +
+"                </getPatientDetails-v1-0>\n" +
+"				</itk:payload>\n" +
+"			</itk:payloads>\n" +
+"		</itk:DistributionEnvelope>\n" +
+"	</soap:Body>\n" +
+"</soap:Envelope>";
+
+requestTemplates[1] = "Req 1";
+requestTemplates[2] = "Req 2";
+requestTemplates[3] = "Req 3";
+requestTemplates[4] = "Req 4";
+requestTemplates[5] = "Req 5";
+requestTemplates[6] = "Req 6";
+requestTemplates[7] = "Req 7";
 
 module.exports = {
 	responseTemplates:responseTemplates,
+	requestTemplates:requestTemplates,
 	errorTemplates:errorTemplates,
 	profiles:profiles,
 	envelopeTemplate:envelopeTemplate
